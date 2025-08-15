139e9ca09b8cc39167e430fccf1c700cb8867f9e
tvk777 <klimenko.tv7@gmail.com>
2025-08-15 17:57:06 +0300

    make build

---
+++ index.1d4b465e.js
@@ -0,0 +1,2 @@
+document.querySelector(".tree").addEventListener("click",function(e){if(e.target.closest("span")){var t=e.target.nextElementSibling;t&&("none"!==t.style.display?t.style.display="none":t.style.display="block")}});
+//# sourceMappingURL=index.1d4b465e.js.map
